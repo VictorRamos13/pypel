4edbda5a7e5c32940bd6e8d71da5f48d7ea61578
Victor Hordones Ramos <victorhordonesramos38@gmail.com>
2025-02-12 17:37:05 -0300

    CRUD departamento completo

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
 
     #CADASTRO DE DEPARTAMENTO
     path('departamentos/', views.departamentos, name='departamentos'),
-    # path('obter_departamento_por_id/', views.obter_departamento_por_id, name='obter_departamento_por_id'),
-    # path('excluir_departamento/', views.excluir_departamento, name='excluir_departamento'),
-    # path('pesquisar_departamento_por_nome/', 
-    #      views.pesquisar_departamento_por_nome, 
-    #      name='pesquisar_departamento_por_nome'
-    #      ),
+    path('obter_departamento_por_id/', views.obter_departamento_por_id, name='obter_departamento_por_id'),
+    path('excluir_departamento/', views.excluir_departamento, name='excluir_departamento'),
+    path('pesquisar_departamento_por_nome/', 
+         views.pesquisar_departamento_por_nome, 
+         name='pesquisar_departamento_por_nome'
+         ),
 
     #-------------------------------------------------------------------------------------------------------------
 
--- cadastros/views.py
+++ cadastros/views.py
@@ -20,9 +20,54 @@ def departamentos(request):
         messages.error(request, 'Você não é administrador!')
         return redirect('core:main') #main é uma view que ta dentro de core
     
+    
+
     if request.method == 'POST':
-        messages.success(request, 'Implementar depois!')
-        #Alguma coisa
+        acao = request.POST.get('btnAcao')
+
+        if acao == 'novo_departamento':
+            nome = request.POST.get('txtName')
+
+            if (nome == 'Geral'):
+                messages.error(request, 'Cadastre outro nome, seu Animal!')
+                return redirect('cadastros:departamentos')
+
+            sigla = request.POST.get('txtSigla')
+
+            if Departamento.objects.filter(nome=nome).exists(): #se existir algum departamento com o nome que o usuario digitou
+                messages.error(request, 'Já existe um departamento com esse nome!')
+                return redirect('cadastros:departamentos')
+            
+            departamento = Departamento(
+                nome=nome,
+                sigla=sigla
+            )
+            departamento.save()
+
+            messages.success(request, 'Departamento cadastrado com sucesso!')
+            return redirect('cadastros:departamentos')
+        
+        elif acao == 'alterar_departamento':
+            departamento_id = request.POST.get('txtId')
+
+            #pega tudo do banco sobre o departamento com base no id
+            departamento = Departamento.objects.get(id=departamento_id)
+
+            nome = request.POST.get('txtName')
+            
+            if (nome == 'Geral' or Departamento.objects.filter(nome=nome).exists()):
+                    messages.error(request, 'Cadastre outro nome, seu Animal!')
+                    return redirect('cadastros:departamentos')
+            
+            sigla = request.POST.get('txtSigla')
+
+            departamento.nome = nome
+            departamento.sigla = sigla
+            departamento.save()
+
+            messages.success(request, 'Departamento alterado com sucesso!')
+            return redirect('cadastros:departamentos')
+
     
     #traz todos os departamentos cadastrados do sistema menos o "geral" e ordena pelo nome
     departamento_lista = Departamento.objects.all().exclude(nome__iexact="Geral").order_by('nome')
@@ -32,4 +77,51 @@ def departamentos(request):
     page_obj = paginator.get_page(numero_pagina)
 
     return render(request, 'departamentos.html', {'page_obj': page_obj})
+
+@login_required
+def obter_departamento_por_id(request):
+    departamento_id = request.GET.get('departamento_id', None)
+
+    #selecione todos os campos do departamento onde o id for igual id_departamento
+    #SELECT * from Departamento WHERE id = id_departamento :)
+    departamento = Departamento.objects.get(id=departamento_id)
+
+    departamento_dados = {
+        'id': departamento.id,
+        'nome': departamento.nome,
+        'sigla': departamento.sigla
+    }
+    return JsonResponse(departamento_dados)
+
+@login_required
+def excluir_departamento(request):
+    if request.method == 'POST':
+        departamento_id = request.POST.get('departamento_id')
+        departamento = Departamento.objects.filter(id=departamento_id).first()
+        departamento.delete()
+
+        if (departamento.usuario_set.exists()):
+            return JsonResponse({'success': False,
+                                 'messages': 'Usuários vinculados!'})
+
+        return JsonResponse({'success': True,
+                            'messages': 'Departamento excluido com sucesso!'})
     
+@login_required
+def pesquisar_departamento_por_nome(request):
+    departamento_nome = request.GET.get('departamento_nome', '')
+    numero_pagina = request.GET.get('page')#do proprio objeto paginator
+
+    #icontains = LIKE no sql
+    departamento_lista = Departamento.objects.filter(nome__icontains=departamento_nome).exclude(nome__iexact="Geral").order_by('nome')
+
+    paginator = Paginator(departamento_lista, settings.NUMBER_GRID_PAGES)
+    page_obj = paginator.get_page(numero_pagina)
+
+    return JsonResponse({
+        #vai renderizar a departamentos_table.html com esses novos parametros
+        'html': render_to_string('departamentos_table.html',
+                                 {'page_obj': page_obj,
+                                  'query': departamento_nome,
+                                  'request': request})
+    })
